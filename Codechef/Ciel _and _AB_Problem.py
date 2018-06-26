a4b8925124e92cc230e8f8e1136fc30c69c97ad4
Shivam sharma <shivams334@gmail.com>
2018-06-26 13:37:26 +0530

    Update Ciel _and _AB_Problem.py

--- Codechef/Ciel _and _AB_Problem.py
+++ Codechef/Ciel _and _AB_Problem.py
@@ -1,1 +1,10 @@
+#Problem statement
+print("https://www.codechef.com/problems/CIELAB/")
 
+#Solution
+number1,number2=map(int,input().split())
+ans=number1-number2
+if answer%10==9:
+    print(answer-1)
+else:
+    print(answer+1)
